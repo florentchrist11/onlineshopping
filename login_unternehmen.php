73304c98cce72d6014a165926e95cdc91f903b05
florentchrist1 <florentchrist1@gmail.com>
2020-05-13 09:53:16 +0200

    testw

---
+++ login_unternehmen.php
@@ -0,0 +1,19 @@
+<?php  require 'elements/header.php'    ?>
+
+  <h1>Login unternehmen</h1>
+  
+    <input type="text" placeholder="Username">
+  </div>
+
+  
+    
+    <input type="password" placeholder="Password">
+  </div>
+
+  <input type="button" class="btn" value="Einloggen">
+</div>
+  
+
+
+
+<?php   require 'elements/footer.php'  ?>
