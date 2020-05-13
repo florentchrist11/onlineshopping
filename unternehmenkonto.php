59a12501317b6a4163c792e48f6c23993eecd895
florentchrist1 <florentchrist1@gmail.com>
2020-05-13 16:57:44 +0200

    change

---
+++ unternehmenkonto.php
@@ -0,0 +1,18 @@
+<?php  require 'elements/header.php'    ?>
+
+  <h1>Login unternehmen</h1>
+   
+  <form action="" class="mb-4">
+       <div class="container"> 
+       <label > <p style="color:blueviolet" >  E-Mail:</p>
+           <input type="text" class="form-control" name="username" placeholder="Name angeben">
+           </label><br>
+           <label > <p style="color:blueviolet" >  Password:</p>
+           <input type="email" class="form-control" name="email" placeholder="Password angeben">
+           </label><br>
+           <label >  <input type="submit" name="create" value="Submit"> </label>
+           </div> 
+ </form>
+  
+
+<?php   require 'elements/footer.php'  ?>
