47f1b60f502b88afbdd2af5af00de72bc737662a
florentchrist1 <florentchrist1@gmail.com>
2020-05-13 09:56:21 +0200

    index

---
+++ index2.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once 'elements/header.php';
+
+
+?>
+
+
+
+
+<h1>Angebote</h1>
+<h5>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</h5>
+<input type ="text" placeholder="Produkt suchen...">
+
+
+
+
+
+
+<?php
+
+require_once('elements/footer.php')
+?>
