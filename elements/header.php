6d0c976d73a229fbd5e0ae019692ec628024b440
florentchrist1 <florentchrist1@gmail.com>
2020-05-13 09:55:29 +0200

    header

---
+++ elements/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+     <link rel="stylesheet" href="style.css">
+     <link ref="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+     <title>Verkaufsbörse</title>
+ </head>
+ <body class="p-4" >
+     <div class="menu-bar">
+     <ul>
+     <li ><a href="#"><i class="fa fa-home"></i>Angebot</a></li>
+     <li ><a href="#"><i class="fa fa-shopping-basket"></i>Warenkorb</a></li>
+     <li ><a href="#"><i class="fa fa-user"></i>Kundenkonto</a>
+     <div class="sub-menu">
+            <ul>
+              <li ><a href="registrieren.php">Registrieren</a></li>
+            </ul>
+            <ul>
+              <li><a href="login.php">Login</a></li>
+             </ul>
+            <ul>
+              <li><a href="login_unternehmen.php">Unternehmenkonto</a></li>
+            </ul>
+           <ul>
+              <li><a href="login_admin.php">Administrator</a></li>
+            </ul>
+            <ul>
+              <li><a href="#">Logout</a></li>
+            </ul>
+     </div>
+    </li>
+     <li ><a href="#"><i class="fa fa-shopping-bag"></i>Bestellung</a></li>
+     </ul>
+    </div>
