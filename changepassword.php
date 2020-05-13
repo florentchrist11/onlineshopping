e78aa6bec902c3a075e614fac1dd5f12359545c0
florentchrist1 <florentchrist1@gmail.com>
2020-05-13 16:56:23 +0200

    change

---
+++ changepassword.php
@@ -0,0 +1,34 @@
+<?php   require 'elements/header.php'        ?>
+
+
+
+
+
+<form action="" class="mb-4">
+       <div class="center"> 
+         <h1 style="color:chartreuse">  Change your password </h1>
+           <label > <p style="color:blueviolet" >  Email:</p>
+           <input type="email" class="form-control" name="Loginemail" placeholder="Enter your E-Mail">
+           </label><br>
+           <label > <p style="color:blueviolet" > New password:</p>
+           <input type="email" class="form-control" name="Loginpassword" placeholder="Enter your Password ">
+           </label><br>
+           <label > <p style="color:blueviolet" > Confirm your password:</p>
+           <input type="email" class="form-control" name="Loginpassword" placeholder="Enter your Password again ">
+           </label><br>
+           <label >  <input type="submit" name="create" value="Sign in"> </label>
+           </div> 
+ </form>
+  
+
+
+
+
+
+
+
+
+
+
+
+<?php     require 'elements/footer.php'                 ?>
