39681ad4185d74d227eed11dccda0fe88178d74d
florentchrist1 <florentchrist1@gmail.com>
2020-05-13 10:21:46 +0200

    login

--- login.php
+++ login.php
@@ -34,18 +34,6 @@
 
 
 
-
-  <form action="">
-  <div class="form-group">
-    <label for="email">Email address</label>
-    <input type="email" class="form-control" id="email">
-  </div>
-  <div class="form-group">
-    <label for="pwd">Password</label>
-    <input type="password" class="form-control" id="pwd">
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-  </form>
 <?php    require('elements/footer.php')        ?>
 
 
