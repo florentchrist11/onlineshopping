e632aa382740cae1b84b3d930893777146c95b96
florentchrist1 <florentchrist1@gmail.com>
2020-05-13 09:57:02 +0200

    change

---
+++ login_admin.php
@@ -0,0 +1,20 @@
+ <?php
+  require('elements/header.php');
+ ?>
+
+  <h1>Login Admin</h1>
+  
+    <input type="text" placeholder="Username">
+  </div>
+
+  
+    
+    <input type="password" placeholder="Password">
+  </div>
+
+  <input type="button" class="btn" value="Einloggen">
+</div>
+  
+
+
+<?php require 'elements/footer.php'      ?>
