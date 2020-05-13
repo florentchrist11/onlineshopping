950f18d0d68c659a5156a471fedfa52828900561
florentchrist1 <florentchrist1@gmail.com>
2020-05-13 09:55:01 +0200

    phpchange

---
+++ registrieren.php
@@ -0,0 +1,16 @@
+ 
+<?php  require('elements/header.php')       ?>
+  <h1>Registrierenen</h1>
+  
+    <input type="text" placeholder="Username">
+  </div>    
+<input type="text" placeholder="Email">
+</div>
+    <input type="password" placeholder="Password">
+  </div>
+
+  <input type="button" class="btn" value="Registieren">
+</div>
+  
+
+<?php   require('elements/footer.php')        ?>
