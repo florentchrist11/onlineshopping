e6224f1a41c9980fb44117d141e337ac748689b4
JBathke-uol <64539078+JBathke-uol@users.noreply.github.com>
2020-05-15 14:24:40 +0200

    Add files via upload
    PHP Code for register and MYSQL connection the script for the database comming soon

--- registrieren.php
+++ registrieren.php
@@ -1,32 +1,60 @@
- 
-<?php  require('elements/header.php')       ?>
-       
-       
-<form action="" class="mb-4">
-       <div class="center"> 
-         <h1 style="color:chartreuse">  Account</h1>
-       <label > <p style="color:blueviolet" >  Your name:</p>
-           <input type="text" class="form-control" name="username" placeholder="Name angeben">
-           </label><br>
-           <label > <p style="color:blueviolet" >  Email:</p>
-           <input type="email" class="form-control" name="email" placeholder="Password angeben">
-           </label><br>
-           <label > <p style="color:blueviolet" >  Password:</p>
-           <input type="email" class="form-control" name="email" placeholder="Password angeben">
-           </label><br>
-           <label > <p style="color:blueviolet" >  Re-enter Password :</p>
-           <input type="email" class="form-control" name="email" placeholder="Password angeben">
-           </label><br>
-           <label >  <input type="submit" name="create" value="Submit"> </label>
-           </div> 
- </form>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Account erstellen</title>
+  </head>
+  <body>
+    <?php
+    if(isset($_POST["submit"])){
+      require("mysql.php");
+      $stmt = $mysql->prepare("SELECT * FROM accounts WHERE USERNAME = :user"); //Username überprüfen
+      $stmt->bindParam(":user", $_POST["username"]);
+      $stmt->execute();
+      $count = $stmt->rowCount();
+      if($count == 0){
+        //Username ist frei
+        $stmt = $mysql->prepare("SELECT * FROM accounts WHERE EMAIL = :email"); //Email überprüfen
+        $stmt->bindParam(":email", $_POST["email"]);
+        $stmt->execute();
+        $count = $stmt->rowCount();
+        if($count == 0){
+          if($_POST["pw"] == $_POST["pw2"]){
+            //User anlegen
+            $stmt = $mysql->prepare("INSERT INTO accounts (USERNAME, PASSWORD, EMAIL, TOKEN) VALUES (:user, :pw, :email, null)");
+            $stmt->bindParam(":user", $_POST["username"]);
+            $hash = password_hash($_POST["pw"], PASSWORD_BCRYPT);
+            $stmt->bindParam(":pw", $hash);
+            $stmt->bindParam(":email", $_POST["email"]);
+            $stmt->execute();
+            echo "Dein Account wurde angelegt";
+          } else {
+            echo "Die Passwörter stimmen nicht überein";
+          }
+        } else {
+          echo "Email bereits vergeben";
+        }
+      } else {
+        echo "Der Username ist bereits vergeben";
+      }
+    }
+     ?>
+    <h1>Account erstellen</h1>
+    <form action="registrieren.php" method="post">
+      <input type="text" name="username" placeholder="Username" required><br>
+      <input type="text" name="email" placeholder="Email" required><br>
+      <input type="password" name="pw" placeholder="Passwort" required><br>
+      <input type="password" name="pw2" placeholder="Passwort wiederholen" required><br>
+      <button type="submit" name="submit">Erstellen</button>
+    </form>
+    <br>
+    <a href="index.php">Bereits Registriert? Dann Logg dich ein!</a>
+  </body>
+</html>
+
+
+
+
+
   
 
-
-
-
-
-
-  
-
-<?php   require('elements/footer.php')        ?>
