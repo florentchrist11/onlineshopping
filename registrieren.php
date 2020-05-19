b64a53a52422bdb97b45ac1f893257134dfbca3c
florentchrist1 <florentchrist1@gmail.com>
2020-05-19 15:56:57 +0200

    change

--- registrieren.php
+++ registrieren.php
@@ -5,19 +5,18 @@
        <form action="" class="mb-4">
               <div class="center"> 
                 <h1 style="color:chartreuse">  Account</h1>
-              <label > <p style="color:blueviolet" >  Your name:</p>
-                  <input type="text" class="form-control" name="username" placeholder="Name angeben">
-                  </label><br>
-                  <label > <p style="color:blueviolet" >  Email:</p>
-                  <input type="email" class="form-control" name="email" placeholder="Password angeben">
-                  </label><br>
-                  <label > <p style="color:blueviolet" >  Password:</p>
-                  <input type="password" class="form-control" name="passwort" placeholder="Password angeben">
-                  </label><br>
-                  <label > <p style="color:blueviolet" >  Re-enter Password :</p>
-                  <input type="password" class="form-control" name="email" placeholder="Password angeben">
-                  </label><br>
-                  <label >  <input type="submit" name="create" value="Submit"> </label>
+              <label for="name" >  Your name:   </label>
+                  <input type="text" class="form-control" name="username" placeholder="Your Name ">
+                  <label for="adress"> Street: </label>
+                  <input type="text" class="form-control" name="street" placeholder="Your Street">
+                  <label for="email" >   Email:    </label>
+                 <input type="email" class="form-control" name="email" placeholder="Your Email">
+                  <label for="pwd" >  Password:  </label>
+                  <input type="password" class="form-control" name="passwort" placeholder="Your Password ">
+                  <label >   Re-enter Password : </label>
+                  <input type="password" class="form-control" name="email" placeholder="Your Passwor">
+                  
+                  <input type="submit" name="create" value="Submit"> 
                   </div> 
         </form>
          
