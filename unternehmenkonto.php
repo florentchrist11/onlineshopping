0c10570ee136593c2e94dab2a742b2459e029100
florentchrist1 <florentchrist1@gmail.com>
2020-05-19 15:57:27 +0200

    change

--- unternehmenkonto.php
+++ unternehmenkonto.php
@@ -5,15 +5,11 @@
   <form action="dashbordSeller.php" class="mb-4">
        <div class="center"> 
        <h1 style="color:chartreuse"> Login Seller</h1>
-           <label > 
-           <p style="color:blueviolet" >  E-Mail:</p>
-           <input type="text" class="form-control" name="username" placeholder="Your E-Mail ">
-           </label><br>
-           <label > 
-           <p style="color:blueviolet" >  CellerID:</p>
+           <label for="email" >  E-Mail:  </label>
+           <input type="email" class="form-control" name="username" placeholder="Your E-Mail ">
+           <label for="pwd" >  </label>
            <input type="password" class="form-control" name="username" placeholder="Your ID">
-           </label><br>
-            <label >  <input type="submit" name="create" value="Submit"> </label>
+       <input type="submit" name="create" value="Submit"> 
            <a href="changepassword.php">password forgotten?</a>
            </div> 
  </form>
