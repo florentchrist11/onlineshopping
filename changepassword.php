bc2b6ab596779e7df2a613b542467806eb3d2c0e
florentchrist1 <florentchrist1@gmail.com>
2020-05-19 15:56:27 +0200

    change

--- changepassword.php
+++ changepassword.php
@@ -3,15 +3,12 @@
 <form action="" class="mb-4">
        <div class="center"> 
          <h1 style="color:chartreuse">  reset password </h1>
-           <label > <p style="color:blueviolet" >  Email:</p>
+         <label for="email">Email address:</label>
            <input type="email" class="form-control" name="Loginemail" placeholder="Enter your E-Mail">
-           </label><br>
-           <label > <p style="color:blueviolet" > New password:</p>
+           <label for="email">Email address:</label>
            <input type="email" class="form-control" name="Loginpassword" placeholder="Enter your Password ">
-           </label><br>
-           <label > <p style="color:blueviolet" > Confirm your password:</p>
-           <input type="email" class="form-control" name="Loginpassword" placeholder="Enter your Password again ">
-           </label><br>
+           <label for="pwd">Password:</label>
+           <input type="pwd" class="form-control" name="Loginpassword" placeholder="Enter your Password again ">
            <label >  <input type="submit" name="create" value="Sign in"> </label>
            </div> 
  </form>
