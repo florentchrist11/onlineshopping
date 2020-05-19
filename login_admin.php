a83f831ffb4b4120bfd1b19abff90bcd0c0bd906
florentchrist1 <florentchrist1@gmail.com>
2020-05-19 15:56:40 +0200

    change

--- login_admin.php
+++ login_admin.php
@@ -5,15 +5,11 @@
   <form action="dashbordSeller.php"  class="mb-4">
           <div class="center"> 
            <h1 style="color:chartreuse">  Login </h1>
-             <label > 
-             <p style="color:blueviolet" >  Email:</p>
+             <label for ="email" >  Email:  </label>
              <input type="email" class="form-control" name="Loginemail" placeholder="Enter your E-Mail">
-             </label><br>
-             <label >
-             <p style="color:blueviolet" >  Password:</p>
+             <label for="password" > Password:   </label> 
              <input type="password" class="form-control" name="Loginpassword" placeholder="Enter your Password ">
-             </label> <br>
-             <label> <input type="submit" name="create" value="Submit"> </label>
+             <input type="submit" name="create" value="Submit"> 
              <a href="changepassword.php">password forgotten?</a> 
           </div>
    </form>
