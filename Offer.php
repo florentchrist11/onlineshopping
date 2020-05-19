66516ced0232c69c9fd2a758cc0e8a60b9b730d7
anne <annelecdoukayou@uni-oldenburg.de>
2020-05-19 13:32:17 +0200

    h

--- Offer.php
+++ Offer.php
@@ -1,19 +1,17 @@
 <?php require_once 'elements/header.php'; ?>
 
 <form action="" class="mb-4">
-       
+      
+
            <label > <input type ="text" placeholder="Produkt suchen..."> </label> 
            <label >  <Input type="submit" name="search" value=" search "> </label>
-        <div > 
-       
+     <div > 
        <h1>Angebote</h1>
        <h5>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</h5>
-       
-       <img src="Bilder/Buch-201100279236.jpg" width="70" height="60" > </img> 
-       
-       </div>
-        
-           
+       <img src=" Bilder/Buch-201100279236.jpg" width ="70" height="60"> </img>
+    </div>
+      
+          
  </form>
 
 
