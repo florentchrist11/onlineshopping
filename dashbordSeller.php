05e4e41f9d4b9c9ca0db76317cc2b92c537ca9f1
JBathke-uol <64539078+JBathke-uol@users.noreply.github.com>
2020-05-29 15:37:53 +0200

    Add files via upload

--- dashbordSeller.php
+++ dashbordSeller.php
@@ -4,33 +4,50 @@
         <div class="rand"> 
            <div class="navbar">
            <a class="glyphicon glyphicon-user"  href="Profil.php"> Profil</a> 
-           <a class="fa fa-bell" style="font-size:24px"> News </a>
-           <a class="fa fa-gift" style="font-size:24px"> Discount</a>
+           <a class="fa fa-bell" style="font-size:24px"> Delete </a>
+           <a class="fa fa-gift" style="font-size:24px"> Update</a>
         </div>
-          <div class="container">
-             <h1>upload pictures </h1>
-              <p>you can load your picture hier!! </p>
-
-              <form action="" class="mb-4">
-           <label  style="color:blueviolet"> Title of the product  </label><br>
-           <input type="text" name="usrname"><br>
-           <label style="color:blueviolet" > Description   </label><br>
-           <textarea rows="4" cols="40" name="comment" form="usrform">
-           </textarea><br>
-           <label style="color:blueviolet" > preis  </label><br>
-               <input type="preis" name="preis">
-          
-           <p> Bitte nur ganzzahlige Beträge eingeben </p><br>
-            
-             
-              <i class="fa fa-camera"> </i> <Input type="file"/> <br>
-              <Input type="submit" name="upload" value="Upload Image"> 
-              </form>
- 
-          </div>
+        <div class="card border-danger">
+            <div class="card-header bg-danger text-white">
+                <strong><i class="fa fa-plus"></i> Add New Product</strong>
+            </div>
+            <div class="card-body">
+                <form action="add.php" method="post">
+                        <div class="form-group col-md-6">
+                            <label for="name" class="col-form-label">Name</label>
+                            <input type="text" class="form-control" id="name" name="name" placeholder="Name" required>
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-md-4">
+                            <label for="price" class="col-form-label">Preis</label>
+                            <input type="number" class="form-control" id="price" name="price" placeholder="Price" required>
+                        </div>
+                        <div class="form-group col-md-4">
+                            <label for="qty" class="col-form-label">Stückzahl</label>
+                            <input type="number" class="form-control" name="qty" id="qty" placeholder="stückzahl" required>
+                        </div>
+                        <div class="form-group col-md-4">
+                            <label for="image" class="col-form-label">Image</label>
+                            <input type="text" class="form-control" name="image" id="image" placeholder="Image URL">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                    <label for="image" class="col-form-label">discription</label>
+                        <textarea name="description" id="" rows="5" class="form-control" placeholder="beschreibung"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-success"><i class="fa fa-check-circle"></i> Hinzufügen</button>
+                </form>
+            </div>
         </div>
 
-    
+
+
+
+        
+
+
+
 
 <?php   require('elements/footer.php')   ?>
 
