12f10d1b2e2ed1fc0467a2a4cc37376cc1bca40c
florentchrist1 <florentchrist1@gmail.com>
2020-06-09 12:32:50 +0200

    HES

--- index.php
+++ index.php
@@ -35,13 +35,13 @@ $stmt = $mysql->prepare('SELECT * FROM Produkt ');
        <input type ="text" id ="Search" placeholder="Search..">
        <button type="search" name="search">Suchen </button> 
            
- </form>
- <div class="centerReg">
+ </form><br><br>
+
       
  <?php  foreach ($result as $k  => $value):  ?>
    
-  
- <div class="row text-center py-5">
+   <div class=" col-md-offset-3">
+ <div class="row text-center py-5"  >
        <div class="col-md-3 col-sm-6 my-3 my-md-0">
        <form action="" method="POST">
        <div class="card shadow">
@@ -67,84 +67,9 @@ $stmt = $mysql->prepare('SELECT * FROM Produkt ');
      </div>
        </form>
        </div>
-       <?php   $k++        ?>
-       <div class="col-md-3 col-sm-6 my-3 my-md-0">
-       <form action="" method="POST">
-       <div class="card shadow">
-       <img src="<?= $value['Myimage'] ; ?>" alt="image" 
-             class="img-fluid card-img-top">          
-               
-                 <div class="card-body">
-                  <h5 class="card-title"><strong><?= $value['username'] ?></strong></h5>
-                  <h6>
-                  <i class='far fa-star' style='font-size:30px;color:yellow' ></i>
-                  <i class='far fa-star' style='font-size:30px;color:yellow'></i>
-                  <i class='far fa-star' style='font-size:30px;color:yellow'></i>
-                  </h6> 
-                  <p class="card-text">
-                  <?= $value['price'] ?>
-                  </p>  
-                  <small><S class="text-secondary">$50</S></small> 
-                  <strong class="price"><?= $value['price'] ?></strong> 
-                  <button type="submit" class="btn btn-warning my-3" name="add"> Add to Card  </button>
-                  <input type="hidden" name="product_id" value="<?= $value['id'] ?>" >
-                  
-               </div> 
-     
-     </div>
-       </form>
        </div>
-       <div class="col-md-3 col-sm-6 my-3 my-md-0">
-       <form action="" method="POST">
-       <div class="card shadow">
-       <img src="<?= $value['Myimage'] ; ?>" alt="image" 
-             class="img-fluid card-img-top">          
-               
-                 <div class="card-body">
-                  <h5 class="card-title"><strong><?= $value['username'] ?></strong></h5>
-                  <h6>
-                  <i class='far fa-star' style='font-size:30px;color:yellow' ></i>
-                  <i class='far fa-star' style='font-size:30px;color:yellow'></i>
-                  <i class='far fa-star' style='font-size:30px;color:yellow'></i>
-                  </h6> 
-                  <p class="card-text">
-                  <?= $value['Idescription'] ?>
-                  </p>  
-                  <small><S class="text-secondary">$50</S></small> 
-                  <strong class="price"><?= $value['price'] ?></strong> 
-                  <button type="submit" class="btn btn-warning my-3" name="add"> Add to Card  </button>
-                  <input type="hidden" name="product_id" value="<?= $value['id'] ?>" >
-               </div> 
-     
-     </div>
-       </form>
+      
        
-       </div>
-       <div class="col-md-3 col-sm-6 my-3 my-md-0">
-       <form action="" method="POST">
-       <div class="card shadow">
-       <img src="<?= $value['Myimage'] ; ?>" alt="image" 
-             class="img-fluid card-img-top">          
-               
-                 <div class="card-body">
-                  <h5 class="card-title"><strong><?= $value['username'] ?></strong></h5>
-                  <h6>
-                  <i class='far fa-star' style='font-size:30px;color:yellow' ></i>
-                  <i class='far fa-star' style='font-size:30px;color:yellow'></i>
-                  <i class='far fa-star' style='font-size:30px;color:yellow'></i>
-                  </h6> 
-                  <p class="card-text">
-                  <?= $value['Idescription'] ?>
-                  </p>  
-                  <small><S class="text-secondary">$50</S></small> 
-                  <strong class="price"><?= $value['price'] ?></strong><br> 
-                  <button type="submit" class="btn btn-warning my-3" name="add"> Add to Card  </button>
-                  <input type="hidden" name="product_id" value="<?= $value['id'] ?>" >
-               </div>
-     
-     </div>
-       </form>
-       </div>   
  
   <?php  endforeach    ?>
 
