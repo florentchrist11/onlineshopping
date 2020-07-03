b56699dbac0b6308dbf9b84351549b0a1ff6cb37
JBathke-uol <64539078+JBathke-uol@users.noreply.github.com>
2020-07-03 18:05:17 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,9 @@
+
+</body>
+<footer class="footer">
+<p><a href="Legal_Privacy.php"> Legal Notice  |  Privacy Policy</a></p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 
+<p><a href="FAQ.php"> FAQ</a></p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 
+      <p>© 2020 Copyright</p>
+ 
+</footer>
+</html>
