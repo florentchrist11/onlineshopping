8332de2a7583a5360e3b2e47d41332cfd6180245
JBathke-uol <64539078+JBathke-uol@users.noreply.github.com>
2020-07-03 18:06:37 +0200

    Add files via upload

---
+++ FAQ.php
@@ -0,0 +1,159 @@
+<?php    require('elements/header.php')        ?>
+<div class="faq">
+<link rel="stylesheet" type="text/css" href="style.css">
+    <br />
+    <br />
+    <br />
+    <br />
+
+    <div class="" id="accordion">
+       <h2> <div class="faqHeader">General questions</div></h2>
+        <div class="card ">
+            <div class="card-header">
+                <h4 class="card-header">
+                    <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne">Is account registration required?</a>
+                </h4>
+            </div>
+            <div id="collapseOne" class="panel-collapse collapse in">
+                <div class="card-block">
+                    Account registration at <strong>SHOP</strong> is only required if you will be selling or buying Products. 
+                </div>
+            </div>
+        </div>
+        <div class="card ">
+            <div class="card-header">
+                <h4 class="card-header">
+                    <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTen">Can i change my Password?</a>
+                </h4>
+            </div>
+            <div id="collapseTen" class="panel-collapse collapse">
+                <div class="card-block">
+                    ofc you can change password at our site 
+                </div>
+            </div>
+        </div>
+        <div class="card ">
+            <div class="card-header">
+                <h4 class="card-header">
+                    <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseEleven">What is the currency used for all transactions?</a>
+                </h4>
+            </div>
+            <div id="collapseEleven" class="panel-collapse collapse">
+                <div class="card-block">
+                    All prices for Products, Books and other items, including each seller's or buyer's account balance are in <strong>USD</strong>
+                </div>
+            </div>
+        </div>
+
+        <h2><div class="faqHeader">Sellers</div></h2>
+        <div class="card ">
+            <div class="card-header">
+                <h4 class="card-header">
+                    <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">Who cen sell items?</a>
+                </h4>
+            </div>
+            <div id="collapseTwo" class="panel-collapse collapse">
+                <div class="card-block">
+                    Any registed user, who want to sell a Product, can post it on <strong>SHOP</strong>.
+                </div>
+            </div>
+        </div>
+        <div class="card ">
+            <div class="card-header">
+                <h4 class="card-header">
+                    <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree">I want to sell my items - what are the steps?</a>
+                </h4>
+            </div>
+            <div id="collapseThree" class="panel-collapse collapse">
+                <div class="card-block">
+                    The steps involved in this process are really simple. All you need to do is:
+                    <ul>
+                        <li>Register an SELLER account</li>
+                        <li>Notice YOUR Seller ID</li>
+                        <li>Go to the <strong>LOGIN </strong> place your E-Mail and SellerID and Press Login</li>
+                        <li>Discribe, set your own Price and upload your Product</li>
+                        
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="card ">
+            <div class="card-header">
+                <h4 class="card-header">
+                    <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseFive">How much do I get from each sale?</a>
+                </h4>
+            </div>
+            <div id="collapseFive" class="panel-collapse collapse">
+                <div class="card-block">
+                    Here, at <strong>SHOP</strong>, we offer a great, 98% rate for each seller, regardless of any restrictions, such as volume, date of entry, etc.
+                    <br />
+                </div>
+            </div>
+        </div>
+        <div class="card ">
+            <div class="card-header">
+                <h4 class="card-header">
+                    <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseSix">Why sell my items here?</a>
+                </h4>
+            </div>
+            <div id="collapseSix" class="panel-collapse collapse">
+                <div class="card-block">
+                    There are a number of reasons why you should join us:
+                    <ul>
+                        <li>A great 98% flat rate for your items.</li>
+                        <li>Fast response/approval times. Many sites take weeks to process a Product. And if it gets rejected, there is another iteration. We have aliminated this, and made the process very fast.</li>
+                        <li>We are not an exclusive marketplace. This means that you can sell your items on <strong>SHOP</strong>, as well as on any other marketplate, and thus increase your earning potential.</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="card ">
+            <div class="card-header">
+                <h4 class="card-header">
+                    <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseEight">What are the payment options?</a>
+                </h4>
+            </div>
+            <div id="collapseEight" class="panel-collapse collapse">
+                <div class="card-block">
+                    The best way to transfer funds is via Paypal. This secure platform ensures timely payments and a secure environment. 
+                </div>
+            </div>
+        </div>
+        <div class="card ">
+            <div class="card-header">
+                <h4 class="card-header">
+                    <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseNine">When do I get paid?</a>
+                </h4>
+            </div>
+            <div id="collapseNine" class="panel-collapse collapse">
+                <div class="card-block">
+                    Our standard payment plan provides for monthly payments. At the end of each month, all accumulated funds are transfered to your account. 
+                    The minimum amount of your balance should be at least 70 USD. 
+                </div>
+            </div>
+        </div>
+
+        <h2> <div class="faqHeader">Buyers</div></h2>
+        <div class="card ">
+            <div class="card-header">
+                <h4 class="card-header">
+                    <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseFour">I want to buy a Product - what are the steps?</a>
+                </h4>
+            </div>
+            <div id="collapseFour" class="panel-collapse collapse">
+                <div class="card-block">
+                    Buying a Product on <strong>SHOP</strong> is really simple. Each theme has a live preview. 
+                    Once you have selected a Product, which is to your liking, you can quickly and securely pay via Paypal.
+                    <br />
+                    Once the transaction is complete, you gain full access shipment tracking
+                </div>
+            </div>
+        </div>  
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php    require('elements/footer.php')        ?>
